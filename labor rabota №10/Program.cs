e46e9dbceb52c7f742f0a18e622ea1dc7d70f055
Kirill Glushankov <kirantiq@gmail.com>
2020-01-06 06:27:29 +0300

    laba 10

---
+++ labor rabota №10/Program.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Collections;
+using System.Collections.Specialized;
+
+namespace labor_rabota__10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Задание
+            /*
+                    Лабораторная работа №10
+                        Коллекции 
+Задание
+1.	Создать необобщенную коллекцию ArrayList. 
+a.	Заполните ее 5-ю случайными целыми числами
+b.	Добавьте к ней строку
+c.	Добавьте объект типа Student
+d.	Удалите заданный элемент
+e.	Выведите количество элементов и коллекцию на консоль.
+f.	Выполните поиск в коллекции значения
+2.	Создать обобщенную коллекцию в соответствии с вариантом задания и заполнить ее данными, тип которых определяется
+вариантом задания (колонка – первый тип). 
+a.	Вывести коллекцию на консоль
+b.	Удалите из коллекции n последовательных элементов 
+c.	Добавьте другие элементы (используйте все возможные методы добавления для вашего типа коллекции). 
+d.	Создайте вторую коллекцию (см. таблицу) и заполните ее данными из первой коллекции. 
+e.	Выведите вторую коллекцию на консоль. В случае не совпадения количества параметров (например, LinkedList<T> и
+Dictionary<Tkey, TValue>), при нехватке  - генерируйте ключи, в случае избыточности – оставляйте TValue.
+f.	Найдите во второй коллекции заданное значение.
+3.	Повторите задание п.2 для пользовательского типа данных (в качестве типа T возьмите любой свой класс из лабораторной №5
+(Наследование…. ). Не забывайте о необходимости реализации интерфейсов (IComparable, ICompare,….). При выводе коллекции
+используйте цикл  foreach.
+4.	Создайте объект наблюдаемой коллекции  ObservableCollection<T>. Создайте  произвольный метод  и зарегистрируйте его 
+на событие CollectionChange. Напишите демонстрацию с добавлением и удалением элементов. В качестве типа T используйте 
+свой класс из лабораторной №5 Наследование….
+
+             */
+            #endregion
+            //1)
+            ArrayList list = new ArrayList();
+            Random rand = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                list.Add(rand.Next(0, 100));
+                Console.Write("{0} ", list[i]);
+            }
+            list.Add("String");
+            object student = new object();
+            list.Add(student);
+            list.RemoveAt(2);
+
+            Console.WriteLine("\nЧисло элементов: {0}", list.Count);
+            Console.WriteLine(list.Contains(student));
+
+            foreach (object listList in list)
+            {
+                Console.Write(" " + listList);
+            }
+            Console.Write("\n\n");
+            //2)
+
+            List<long> ListL = new List<long>();
+            for (int i = 0; i < 10; i++)
+            {
+                ListL.Add(rand.Next(0, 100));
+            }
+
+
+            int n = 4;
+            ListL.RemoveRange(3, n);    // от 3 индекса удаляет n(4) элемента
+            ListL.Add(rand.Next(0, 111));
+
+            SortedSet<long> Sorset = new SortedSet<long>();
+
+            foreach (long lll in ListL)
+            {
+                Sorset.Add(lll);
+                Console.Write(lll + " ");
+            }
+
+            Console.WriteLine("\n");
+            foreach (long sor in Sorset)
+            {
+                Console.Write(sor + " ");
+            }
+            long x;
+
+            Sorset.TryGetValue(14, out x);
+            Sorset.Contains(14);
+            Console.WriteLine("\n");
+            // 3)
+            List<Transport> ListTrans = new List<Transport>();
+            Boat boat1 = new Boat("default");
+            Sailboat sailboat1 = new Sailboat("default");
+            Steamboat steamboat1 = new Steamboat("default");
+            Corvette corve1 = new Corvette("default");
+            Ship ship1 = new Ship("defautl");
+            ListTrans.Add(sailboat1);
+            ListTrans.Add(boat1);
+            ListTrans.Add(steamboat1);
+            ListTrans.Add(corve1);
+            ListTrans.Add(ship1);
+
+            foreach (var spisTrans in ListTrans)
+            {
+                Console.Write(spisTrans.GetType()); Console.WriteLine("\t" + spisTrans.Name);
+            }
+
+            ListTrans.RemoveRange(1, 2);    // от 1 индекса удаляет 2 элемента
+            ListTrans.Add(boat1);
+            ListTrans.Add(steamboat1);
+            Console.WriteLine("\n");
+            SortedSet<Transport> SorTrans = new SortedSet<Transport>();
+
+            foreach (var spisTrans in ListTrans)
+            {
+                SorTrans.Add(spisTrans);
+                Console.Write(spisTrans.Name + " ");
+            }
+
+            Console.WriteLine("\n");
+
+            foreach (var spisTrans in SorTrans)
+            {
+                Console.Write(spisTrans.Name + " ");
+            }
+
+            SorTrans.Contains(boat1);
+            Console.WriteLine("\n");
+
+            //4)
+
+            ObservableCollection<Transport> transObserv = new ObservableCollection<Transport>();
+
+            transObserv.CollectionChanged += TransObserv_CollectionChanged;
+            transObserv.Add(sailboat1);
+            transObserv.Add(ship1);
+            transObserv.Add(boat1);
+            transObserv.Add(steamboat1);
+            transObserv.Add(corve1);
+            transObserv.Remove(corve1);
+            transObserv.Remove(sailboat1);
+ 
+
+            Console.ReadKey();
+        }
+
+        private static void TransObserv_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Add:         // если добавление
+                    Transport newShip = e.NewItems[0] as Transport;
+                    Console.WriteLine($"Добавлен новый корабль: {newShip.Name}");
+                    break;
+                case NotifyCollectionChangedAction.Remove: // если удаление
+                    Transport oldShip = e.OldItems[0] as Transport;
+                    Console.WriteLine($"Удален корабль: {oldShip.Name}");
+                    break;
+            }
+        }
+    }
+}
---
+++ labor rabota №10/laba_5.cs
@@ -0,0 +1,377 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labor_rabota__10
+{
+    abstract public class Transport :IComparable 
+    {
+        private string name;
+        private int speed;
+        private int crew;
+        private string madeOf;
+        private string engineType;
+        public bool capStatus = false;
+        public static int count = 0;
+
+        public int CompareTo(object obj)
+        {
+            Transport p = obj as Transport;
+            if (p != null)
+                return this.Name.CompareTo(p.Name);
+            else
+                throw new Exception("Невозможно сравнить два объекта");
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value == "" || value == null) name = "Кораблик №" + count;
+                else name = value;
+            }
+        }
+
+        public int Speed        //аксессоры
+        {
+            get { return speed; }
+            set
+            {
+                if (value < 1) speed = 1;
+                else speed = value;
+            }
+        }
+        public int Crew
+        {
+            get { return crew; }
+            set
+            {
+                if (value < 1) crew = 1;
+                else crew = value;
+            }
+        }
+        public string MadeOf
+        {
+            get { return madeOf; }
+            set { madeOf = value; }
+        }
+        public string EngineType
+        {
+            get { return engineType; }
+            set { engineType = value; }
+        }
+
+        public string nameCap;
+        public byte ageCap;
+        public byte termCap;
+
+        public virtual void Constructor()
+        {
+            count++;
+            this.Name = Name;
+            this.Speed = Speed;
+            this.Crew = Crew;
+            this.MadeOf = MadeOf;
+            this.EngineType = EngineType;
+
+        }
+        public virtual void Constructor(string _default)
+        {
+            count++;
+            Name = "Unknown ship №" + count;
+            Speed = 40;
+            Crew = 14;
+            MadeOf = "Steel";
+            EngineType = "Unknown";
+        }
+        public virtual void Constructor(string name, int speed, int crew, string madeOf, string engineType)
+        {
+            count++;
+            this.Name = name;
+            this.Speed = speed;
+            this.Crew = crew;
+            this.MadeOf = madeOf;
+            this.EngineType = engineType;
+        }
+        public virtual void appointCap(Capitan capTan)
+        {
+            if (capStatus == true) Console.WriteLine("На корабле уже есть капитан");
+            if (capTan.BusyCap == true) Console.WriteLine("Капитан уже работает на другом корабле");
+            if (capStatus == false && capTan.BusyCap == false)
+            {
+                capStatus = true;
+                capTan.BusyCap = true;
+                nameCap = capTan.NameCap;
+                ageCap = capTan.AgeCap;
+                termCap = capTan.TermCap;
+            }
+
+
+        }
+
+        public virtual void Print()
+        {
+            Console.WriteLine("Имя транспорта: {0}", Name);
+            Console.WriteLine("Скорость транспорта: {0}", Speed);
+            Console.WriteLine("Экипаж транспорта: {0}", Crew);
+            Console.WriteLine("Материал транспорта: {0}", MadeOf);
+            Console.WriteLine("Тип двигателя транспорта: {0}", EngineType);
+            Console.WriteLine("Капитан водного транспорта: {0}\n\n", capStatus);
+        }
+    }
+
+    sealed class Ship : Transport         //класс корабль
+    {
+        public Ship()       //конструктор
+        {
+            base.Constructor();
+        }
+        public Ship(string _default)                    //конструктор по умолчанию
+        {
+            base.Constructor("default");
+        }
+        public Ship(string name, int speed, int crew, string madeOf, string engineType)
+        {
+            base.Constructor(name, speed, crew, madeOf, engineType);
+        }
+
+        public override string ToString()
+        {
+            Console.WriteLine("Имя транспорта: {0}", Name);
+            Console.WriteLine("Скорость транспорта: {0}", Speed);
+            Console.WriteLine("Экипаж транспорта: {0}", Crew);
+            Console.WriteLine("Материал транспорта: {0}", MadeOf);
+            Console.WriteLine("Тип двигателя транспорта: {0}", EngineType);
+            Console.WriteLine("Капитан водного транспорта: {0}\n", capStatus);
+            if (capStatus == true)
+            {
+                Console.WriteLine("Информация о капитане: ");
+                Console.WriteLine("Имя: {0}", nameCap);
+                Console.WriteLine("Возраст: {0}", ageCap);
+                Console.WriteLine("Срок службы: {0}\n", termCap);
+            }
+            return base.ToString();
+        }
+    }
+
+    sealed class Steamboat : Transport      //класс параход
+    {
+        public Steamboat()
+        {
+            base.Constructor();
+        }
+
+        public Steamboat(string _default)
+        {
+            base.Constructor("default");
+        }
+
+        public Steamboat(string name, int speed, int crew, string madeOf, string engineType)
+        {
+            base.Constructor(name, speed, crew, madeOf, engineType);
+        }
+
+        public override string ToString()
+        {
+            Console.WriteLine("Имя транспорта: {0}", Name);
+            Console.WriteLine("Скорость транспорта: {0}", Speed);
+            Console.WriteLine("Экипаж транспорта: {0}", Crew);
+            Console.WriteLine("Материал транспорта: {0}", MadeOf);
+            Console.WriteLine("Тип двигателя транспорта: {0}", EngineType);
+            Console.WriteLine("Капитан водного транспорта: {0}\n", capStatus);
+            if (capStatus == true)
+            {
+                Console.WriteLine("Информация о капитане: ");
+                Console.WriteLine("Имя: {0}", nameCap);
+                Console.WriteLine("Возраст: {0}", ageCap);
+                Console.WriteLine("Срок службы: {0}\n", termCap);
+            }
+            return base.ToString();
+        }
+    }
+
+    sealed class Sailboat : Transport      //класс парусник
+    {
+        public Sailboat()
+        {
+            base.Constructor();
+        }
+
+        public Sailboat(string _default)
+        {
+            base.Constructor("default");
+        }
+
+        public Sailboat(string name, int speed, int crew, string madeOf, string engineType)
+        {
+            base.Constructor(name, speed, crew, madeOf, engineType);
+        }
+
+        public override string ToString()
+        {
+            Console.WriteLine("Имя транспорта: {0}", Name);
+            Console.WriteLine("Скорость транспорта: {0}", Speed);
+            Console.WriteLine("Экипаж транспорта: {0}", Crew);
+            Console.WriteLine("Материал транспорта: {0}", MadeOf);
+            Console.WriteLine("Тип двигателя транспорта: {0}", EngineType);
+            Console.WriteLine("Капитан водного транспорта: {0}\n", capStatus);
+            if (capStatus == true)
+            {
+                Console.WriteLine("Информация о капитане: ");
+                Console.WriteLine("Имя: {0}", nameCap);
+                Console.WriteLine("Возраст: {0}", ageCap);
+                Console.WriteLine("Срок службы: {0}\n", termCap);
+            }
+            return base.ToString();
+        }
+    }
+
+    sealed class Corvette : Transport
+    //класс корвет
+    {
+        public Corvette()
+        {
+            base.Constructor();
+        }
+
+        public Corvette(string _default)
+        {
+            base.Constructor("default");
+        }
+
+        public Corvette(string name, int speed, int crew, string madeOf, string engineType)
+        {
+            base.Constructor(name, speed, crew, madeOf, engineType);
+        }
+
+        public override string ToString()
+        {
+            Console.WriteLine("Имя транспорта: {0}", Name);
+            Console.WriteLine("Скорость транспорта: {0}", Speed);
+            Console.WriteLine("Экипаж транспорта: {0}", Crew);
+            Console.WriteLine("Материал транспорта: {0}", MadeOf);
+            Console.WriteLine("Тип двигателя транспорта: {0}", EngineType);
+            Console.WriteLine("Капитан водного транспорта: {0}\n", capStatus);
+            if (capStatus == true)
+            {
+                Console.WriteLine("Информация о капитане: ");
+                Console.WriteLine("Имя: {0}", nameCap);
+                Console.WriteLine("Возраст: {0}", ageCap);
+                Console.WriteLine("Срок службы: {0}\n", termCap);
+            }
+            return base.ToString();
+        }
+    }
+
+    sealed class Boat : Transport      //класс лодка
+    {
+        public Boat()
+        {
+            Constructor();
+        }
+
+        public Boat(string _default)
+        {
+            Constructor("default");
+        }
+
+        public Boat(string name, int speed, int crew, string madeOf, string engineType)
+        {
+            Constructor(name, speed, crew, madeOf, engineType);
+        }
+
+        public override string ToString()
+        {
+            Console.WriteLine("Имя транспорта: {0}", Name);
+            Console.WriteLine("Скорость транспорта: {0}", Speed);
+            Console.WriteLine("Экипаж транспорта: {0}", Crew);
+            Console.WriteLine("Материал транспорта: {0}", MadeOf);
+            Console.WriteLine("Тип двигателя транспорта: {0}", EngineType);
+            Console.WriteLine("Капитан водного транспорта: {0}\n", capStatus);
+            if (capStatus == true)
+            {
+                Console.WriteLine("Информация о капитане: ");
+                Console.WriteLine("Имя: {0}", nameCap);
+                Console.WriteLine("Возраст: {0}", ageCap);
+                Console.WriteLine("Срок службы: {0}\n", termCap);
+            }
+            return base.ToString();
+        }
+    }
+     public class Capitan
+    {
+        private string nameCap;
+        private byte ageCap;
+        private byte termCap;
+        private bool busyCap;
+        public static int countCap = 0;
+
+        public string NameCap
+        {
+            get { return nameCap; }
+            set { nameCap = value; }
+        }
+
+        public byte AgeCap
+        {
+            get { return ageCap; }
+            set
+            {
+                if (value < 1) ageCap = 1;
+                else ageCap = value;
+            }
+        }
+
+        public byte TermCap
+        {
+            get { return termCap; }
+            set
+            {
+                if (value < 1) termCap = 1;
+                else termCap = value;
+            }
+        }
+
+        public bool BusyCap
+        {
+            get { return busyCap; }
+            set { busyCap = value; }
+        }
+
+        public Capitan()
+        {
+            countCap++;
+            this.NameCap = NameCap;
+            this.AgeCap = AgeCap;
+            this.TermCap = TermCap;
+            this.BusyCap = BusyCap;
+        }
+        public Capitan(string _default)
+        {
+            countCap++;
+            NameCap = "Capitan №" + countCap;
+            AgeCap = 43;
+            TermCap = 5;
+            BusyCap = false;
+        }
+        public Capitan(string nameCap, byte ageCap, byte termCap)
+        {
+            countCap++;
+            NameCap = nameCap;
+            AgeCap = ageCap;
+            TermCap = termCap;
+            BusyCap = false;
+        }
+        public override string ToString()
+        {
+            Console.WriteLine("Имя капитана: {0}", NameCap);
+            Console.WriteLine("Возраст капитана: {0}", AgeCap);
+            Console.WriteLine("Срок службы: {0}", TermCap);
+            Console.WriteLine("Есть ли у капитана корабль: {0}\n", BusyCap);
+            return base.ToString();
+        }
+    }
+}
